5b9e332a32759ec006365fbfc82302742ec21e0c
Rahmaniatok <seseorangnia@gmail.com>
2023-02-26 18:52:32 +0700

    Memindah login ke file login.tsx menambahkan signup page (dari punya Billy).

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,9 @@ import Head from 'next/head'
 
 export default function Home(){
   return (
-    <div className="flex flex-col item-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div className="bg-white justify-center">
-          {/* Logo */}
-          <img class="h-205 w-206 justify-center rounded-lg" src="/logo.svg" ></img>
-          {/* Username */}
-          <div>
-            <p class="text-left font-['Inter'] text-sm">Username</p>
-            <input class="bg-white w-226 h-30 rounded-md border-2 border-[#E89005] flex"></input>
-          </div>
-          {/* Password */}
-          <div>
-            <p class="text-left font-['Inter'] text-sm">Password</p>
-            <input class="bg-white w-226 h-30 rounded-md border-2 border-[#E89005] flex"></input>
-            <p class="text-right text-[#D84A05] font-['Inter'] text-xs pr-8">Forgot Password</p>
-          </div>
-          {/* tombol login */}
-          <div>
-            <div class="pt-8"></div>
-            <a href="a" class="bg-[#EC7505] rounded-md px-6 py-3 text-white font-['Inter'] text-base">LOGIN</a>
-            <div class="pt-8"></div>
-          </div>
-          {/* tidak punya akun */}
-          <div class="flex flex-row ">
-            <p class="font-['Inter']">Doesn't have an account?</p>
-            <a href="a" class="text-bold font-['Inter'] font-bold">SIGN UP</a>
-          </div>
-        </div>
-      </main>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Ga tau cara bikin komponen gan, yang dah jadi /login
+      <a href="login">Login</a>
     </div>
   )
 }
---
+++ pages/login.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+
+export default function Home(){
+  return (
+    <div className="flex flex-col item-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="bg-white justify-center">
+          {/* Logo */}
+          <div className="flex justify-center py-6">
+            <img className="h-205 w-206 justify-center rounded-lg" src="/logo.svg" ></img>
+          </div>
+          {/* Username */}
+          <div className="p-2">
+            <p className="text-left font-bold text-sm">Username</p>
+            <input className="bg-white w-226 h-30 rounded-md border-2 border-[#E89005] flex justify-center"></input>
+          </div>
+          {/* Password */}
+          <div className="p-2">
+            <p className=" font-bold text-left text-sm">Password</p>
+            <input className="bg-white w-226 h-30 rounded-md border-2 border-[#E89005] flex"></input>
+            <p className="text-right text-[#D84A05] font-bold text-xs ">Forgot Password</p>
+          </div>
+          {/* tombol login */}
+          <div>
+            <div className="py-2">
+              <button className="bg-[#EC7505] text-white text-[20px] px-4 py-2 rounded font-bold">
+                LOGIN
+              </button>
+            </div>
+          </div>
+          {/* tidak punya akun */}
+          <div className="flex flex-row justify-center py-2">
+            <p className="text-[12px]">Doesn't have an account? <a href="signup" className="text-bold text-[12px] font-bold">SIGN UP</a>
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
