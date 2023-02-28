9c1504ed7374699cca9c815b04a0c9cb69aed479
billyqodama <88262492+billyqodama@users.noreply.github.com>
2023-02-28 17:55:17 +0700

    Menambahkan Components untuk Transaksi Seller
    Masih kurang navbar

---
+++ components/Hari.tsx
@@ -0,0 +1,9 @@
+export default function Hari () {
+    return (
+        <div className="flex justify-between">
+            <div className="p-4 px-8 sm:mx-auto">
+                <h1 className="font-bold font-inter text-xl">Hari ini</h1>
+            </div>
+        </div>
+    )
+}
---
+++ components/Pendapatan.tsx
@@ -0,0 +1,10 @@
+export default function Pendapatan () {
+    return (
+        <div className="flex flex-col">
+            <h1 className="m-5 font-bold font-inter text-base sm:mx-auto">Total Pendapatan Hari Ini</h1>
+            <div className="rounded-lg bg-[#E89006] flex justify-center mx-auto w-1/2">
+                <h2 className="font-bold font-inter text-2xl p-6">15.000</h2>
+            </div>
+        </div>
+    )
+}
