3adf734025e72f4736aceb6483805be8ae5e649c
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-03-01 20:17:55 +0700

    add font-inter and text-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      display: ["Satisfy", "cursive"],
+      fontFamily:{
+        inter: ['Inter', 'sans-serif'],
+        display: ["Satisfy", "cursive"],
+      },
+
+    fontSize: {
+      xmobile:"0.596rem",
+      mobile:"0.688rem",
+      sm: '0.8rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     },
   },
   plugins: [],
