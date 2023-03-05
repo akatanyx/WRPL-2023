a953c37d77b863efa4f5a43c8e768ac532fb1cf2
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-03-06 01:43:44 +0700

    Create Wavebawah.tsx
    Add a components for bottom wave background

---
+++ components/Wavebawah.tsx
@@ -0,0 +1,14 @@
+export default function Wave_bg () {
+    return (
+        <footer >
+        <img 
+            className="absolute w-full -z-10 "
+            src="wave_bawah1.svg"
+            ></img>
+            <img 
+            className="absolute w-full -z-10"
+            src="wave_bawah2.svg"
+            ></img>
+        </footer>
+    )
+  }
