bb4c94350efed82ca1b54d91ec46f030a2b87866
Rahmaniatok <seseorangnia@gmail.com>
2023-03-06 18:40:39 +0700

    wave bawah biar nempel

--- components/Wavebawah.tsx
+++ components/Wavebawah.tsx
@@ -2,11 +2,11 @@ export default function Wavebawah () {
     return (
         <footer >
         <img 
-            className="absolute w-full -z-10 "
+            className="absolute bottom-16 w-full -z-10 "
             src="wave_bawah1.svg"
             ></img>
             <img 
-            className="absolute w-full -z-10"
+            className="absolute bottom-16 w-full -z-10"
             src="wave_bawah2.svg"
             ></img>
         </footer>
