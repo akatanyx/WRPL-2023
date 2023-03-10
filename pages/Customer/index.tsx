6fd7578f0121a6218a92f00e79457a3466fda618
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-03-10 19:19:32 +0700

    Update index.tsx

--- pages/Customer/index.tsx
+++ pages/Customer/index.tsx
@@ -20,17 +20,25 @@ export default function c_landing () {
         </div>
 
         {/* Search */}  
-        <form className="flex items-center px-4 ">   
-            <div className="relative w-full drop-shadow-2xl">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <img src="icon_search.svg" alt="" />
+        <div className="flex justify-center">
+            <form className="flex items-center px-4 w-3/4 ">   
+                <div className="relative w-full drop-shadow-2xl">
+                    <div className="absolute inset-y-0 
+                        left-0 flex items-center pl-3 pointer-events-none">
+                        <img src="icon_search.svg" alt="" />
+                    </div>
+                    <input 
+                        type="text"  
+                        placeholder="Search" 
+                        required 
+                        className="bg-white text-black font-poppins 
+                            text-xl rounded-3xl block w-full pl-10 p-2.5 border 
+                            border-black border-opacity-20 focus:outline-none"
+                        />
                 </div>
-                <input type="text" className="bg-white text-black font-poppins 
-                    text-sm rounded-3xl block w-full pl-10 p-2.5 " placeholder="Search" 
-                    required />
-            </div>
-        </form>
-
+            </form>
+        </div>
+        
         {/* Promo */}
         <div className="p-11">
             <Promo /> 
