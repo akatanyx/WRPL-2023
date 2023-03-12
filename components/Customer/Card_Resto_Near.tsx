a38704667ffd33e37a0a6339306e6378ab9081cd
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-03-13 01:03:33 +0700

    Add some components for landing page
    Masih Hard Coded dan belum mengaplikasikan swiper

---
+++ components/Customer/Card_Resto_Near.tsx
@@ -0,0 +1,11 @@
+export default function Card_Resto_Near () {
+    return (
+        <>   
+            <div className="flex-col shadow-xl rounded-lg">
+                    <img src="icon_landing_restaurant.svg" alt="" />
+                    <h1 className="font-poppins font-semibold text-sm text-center">Anteiku Coffee</h1>
+                    <img src="landing_rate_meter.svg" alt="" className="mx-auto"/>
+                </div>
+        </>
+    )
+}
--- components/Customer/SlidePromo.js
+++ components/Customer/SlidePromo.js
@@ -1,11 +1,15 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Navigation, Pagination } from 'swiper';
 import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/swiper-bundle.css';
 
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
+SwiperCore.use([Pagination]);
 
 export default function SlidePromo () {
   
@@ -26,8 +30,8 @@ export default function SlidePromo () {
         // Setingan efek coverflow
         coverflowEffect={{
           rotate: 0,
-          stretch: 100,
-          depth: 0,
+          stretch: 10,
+          depth: 100,
           modifier: 2.5,
           slideShadows: false,
           }}
@@ -39,10 +43,15 @@ export default function SlidePromo () {
             disableOnInteraction: false
           }}
 
-        pagination = {{ clickable: true }}
-        navigation = {{ clickable: true }}
-        modules={[EffectCoverflow, Navigation, Pagination, Autoplay]}
+        pagination={{
+            clickable: true,
+            renderBullet: (i, classname) => {
+              return `<div class="${classname} w-4 h-8 sm:w-6 sm:h-10 bg-pagination bg-no-repeat bg-contain bg-center"></div>`
+            },
+          }}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
         className="h-full p-8 relative"
+
       >
         <SwiperSlide>
           <img src='../promo1.svg' alt="slide_image" />
@@ -62,6 +71,9 @@ export default function SlidePromo () {
         <SwiperSlide>
           <img src='../promo1.svg' alt="slide_image" />
         </SwiperSlide>
+
+        <div className='flex justify-center mt-10 relative bottom-8 w-4' />
+
       </Swiper>
     </div>
   );
