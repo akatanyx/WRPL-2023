ba9aacad11a2006b50a1e0a99cae4c190c8395d0
Rahmaniatok <seseorangnia@gmail.com>
2023-03-19 21:55:14 +0700

    update login

--- pages/login.tsx
+++ pages/login.tsx
@@ -20,73 +20,69 @@
 //     console.log(res);
 //     Router.replace("/");
 //   };
-//   return (
-//     <div className="flex flex-col item-center justify-center min-h-screen py-2">
-//       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-//         <div className="bg-white justify-center">
-//           {/* Logo */}
-//           <div className="flex justify-center py-6">
-//             <img
-//               className="h-205 w-206 justify-center rounded-lg"
-//               src="/logo.svg"
-//             ></img>
-//           </div>
-//           <form onSubmit={handleSubmit}>
-//             {/* email */}
-//             <div className="p-2">
-//               <p className="text-left font-bold text-sm">Email</p>
-//               <input
-//                 value={userInfo.email}
-//                 onChange={({ target }) =>
-//                   setUserInfo({ ...userInfo, email: target.value })
-//                 }
-//                 type="email"
-//                 placeholder="Email"
-//                 className="p-1 bg-white w-226 h-30 rounded-md border-2 border-[#E89005] flex justify-center"
-//               ></input>
-//             </div>
-//             {/* Password */}
-//             <div className="p-2">
-//               <p className=" font-bold text-left text-sm">Password</p>
-//               <input
-//                 value={userInfo.password}
-//                 onChange={({ target }) =>
-//                   setUserInfo({ ...userInfo, password: target.value })
-//                 }
-//                 type="password"
-//                 placeholder="********"
-//                 className="p-1 bg-white w-226 h-30 rounded-md border-2 border-[#E89005] flex"
-//               ></input>
-//               <p className="text-right text-[#D84A05] font-bold text-xs ">
-//                 Forgot Password
-//               </p>
-//             </div>
-//             {/* tombol login */}
-//             <div>
-//               <div className="py-2">
-//                 <button
-//                   type="submit"
-//                   value="Login"
-//                   className="bg-[#EC7505] hover:bg-orange-700 text-white text-[20px] px-4 py-2 rounded font-bold"
-//                 >
-//                   LOGIN
-//                 </button>
-//               </div>
-//             </div>
-//           </form>
-//           {/* tidak punya akun */}
-//           <div className="flex flex-row justify-center py-2">
-//             <p className="text-[12px]">
-//               Doesn't have an account?{" "}
-//               <a href="signup_page" className="text-bold text-[12px] font-bold">
-//                 SIGN UP
-//               </a>
-//             </p>
-//           </div>
-//         </div>
-//       </main>
-//     </div>
-//   );
+  // return (
+  //   <div className="flex flex-col item-center justify-center min-h-screen py-2">
+  //     <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+  //       <div className="bg-white justify-center">
+  //         {/* Logo */}
+  //         <div className="flex justify-center py-6">
+  //           <img
+  //             className="h-205 w-206 justify-center rounded-lg"
+  //             src="/logo.svg"
+  //           ></img>
+  //         </div>
+  //         <form onSubmit={handleSubmit}>
+  //           {/* email */}
+  //           <div className="p-2">
+  //             <p className="text-left font-bold text-sm">Email</p>
+  //                 <label htmlFor="email">Email address</label>
+  //                 <input
+  //                   type="email"
+  //                   id="email"
+  //                   value={email}
+  //                   onChange={(e) => setEmail(e.target.value)}
+  //                 />
+  //           </div>
+  //           {/* Password */}
+  //           <div className="p-2">
+  //             <p className=" font-bold text-left text-sm">Password</p>
+  //                 <label htmlFor="password">Password</label>
+  //                 <input
+  //                   type="password"
+  //                   id="password"
+  //                   value={password}
+  //                   onChange={(e) => setPassword(e.target.value)}
+  //                   />
+  //             <p className="text-right text-[#D84A05] font-bold text-xs ">
+  //               Forgot Password
+  //             </p>
+  //           </div>
+  //           {/* tombol login */}
+  //           <div>
+  //             <div className="py-2">
+  //               <button
+  //                 type="submit"
+  //                 value="Login"
+  //                 className="bg-[#EC7505] hover:bg-orange-700 text-white text-[20px] px-4 py-2 rounded font-bold"
+  //               >
+  //                 LOGIN
+  //               </button>
+  //             </div>
+  //           </div>
+  //         </form>
+  //         {/* tidak punya akun */}
+  //         <div className="flex flex-row justify-center py-2">
+  //           <p className="text-[12px]">
+  //             Doesn't have an account?{" "}
+  //             <a href="signup_page" className="text-bold text-[12px] font-bold">
+  //               SIGN UP
+  //             </a>
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </main>
+  //   </div>
+  // );
 // };
 
 
@@ -115,25 +111,65 @@ export default function SignIn() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email address</label>
-      <input
-        type="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit">Sign in</button>
-    </form>
+    <div className="flex flex-col item-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="bg-white justify-center">
+          {/* Logo */}
+          <div className="flex justify-center py-6">
+            <img
+              className="h-205 w-206 justify-center rounded-lg"
+              src="/logo.svg"
+            ></img>
+          </div>
+          <form onSubmit={handleSubmit}>
+            {/* email */}
+            <div className="p-2">
+              <p className="text-left font-bold text-sm">Email</p>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+            </div>
+            {/* Password */}
+            <div className="p-2">
+              <p className=" font-bold text-left text-sm">Password</p>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+              <p className="text-right text-[#D84A05] font-bold text-xs ">
+                Forgot Password
+              </p>
+            </div>
+            {/* tombol login */}
+            <div>
+              <div className="py-2">
+                <button
+                  type="submit"
+                  value="Login"
+                  className="bg-[#EC7505] hover:bg-orange-700 text-white text-[20px] px-4 py-2 rounded font-bold"
+                >
+                  LOGIN
+                </button>
+              </div>
+            </div>
+          </form>
+          {/* tidak punya akun */}
+          <div className="flex flex-row justify-center py-2">
+            <p className="text-[12px]">
+              Doesn't have an account?{" "}
+              <a href="signup_page" className="text-bold text-[12px] font-bold">
+                SIGN UP
+              </a>
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 
