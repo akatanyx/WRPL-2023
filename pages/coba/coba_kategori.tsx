b59a4a81fbcc665c235750765c4d72cbf7a990ec
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-03-20 02:41:37 +0700

    fix_add kategori

--- pages/coba/coba_kategori.tsx
+++
@@ -1,9 +0,0 @@
-import Tambah_Kategori from "../../components/coba/Tambah_Kategori";
-
-export default function coba_kategori() {
-  return (
-    <div>
-      <Tambah_Kategori />
-    </div>
-  );
-}
--- pages/merchant/add_kategori.tsx
+++ pages/merchant/add_kategori.tsx
@@ -1,108 +1,90 @@
-import List_Kategori from "@/components/Merchant/Add_Kategori/List_Kategori"
+import { useState } from "react";
 
-export default function add_kategori () {
-    return (
-        <>
-            <div className="bg-[#EC7505] h-screen font-poppins">
+const add_kategori = () => {
+    
+  const daftarKategori = ["Ayam", "Daging", "Babi", "Sayuran", "Buah", "Kopi", "Susu", "Telur", 
+  "Ikan", "Bumbu", "Minuman", "Makanan Ringan", "Makanan Berat", "Makanan Sehat", "Makanan Khas", 
+  "Makanan Tradisional"];
+
+  const [kategoriTerpilih, setKategoriTerpilih] = useState<string[]>([]);
+
+  const tambahKategoriTerpilih = (kategori: string) => {
+    if (!kategoriTerpilih.includes(kategori)) {
+      setKategoriTerpilih([...kategoriTerpilih, kategori]);
+    }
+  };
+
+  const hapusKategoriTerpilih = (kategori: string) => {
+    setKategoriTerpilih(kategoriTerpilih.filter((k) => k !== kategori));
+  };
+
+  return (
+
+    <>
+      <div className="bg-[#EC7505] h-screen font-poppins">
                 
-                {/* Judul */}
-                <div className="px-[19px] py-[19px]">
-                    <h1 className="font-semibold text-[32px] text-white">
-                        Kategori
-                    </h1>
-                </div>
-                
-                {/* List Kategori */}
-                <div className="bg-white h-screen rounded-t-3xl flex-col ">
+        {/* Judul */}
+        <div className="px-[19px] py-[19px]">
+          <h1 className="font-semibold text-[32px] text-white">
+            Kategori
+          </h1>
+        </div>
+
+        {/* List Kategori */}
+        <div className="bg-white h-screen rounded-t-3xl flex-col ">
                     
-                    <div className="px-[30px] py-4">
-                        <h1 className="font-semibold text-xl">
-                            Kategori yang dipilih
-                        </h1>
-                    </div>
-
-                                     
-                    <div className="px-[30px] justify-between flex flex-wrap gap-x-1 gap-y-3">
-
-                        <List_Kategori>
-                            Ayam
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Kopi
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Sapi
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Babi
-                        </List_Kategori>
-                        
-                        <List_Kategori>
-                            Sayuran
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Buah
-                        </List_Kategori>
-                        
-                        <List_Kategori>
-                            Kambing
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Bekicot
-                        </List_Kategori>
-
-                    </div>
-
-                    <div className="px-[30px] py-4">
-                        <h1 className="font-semibold text-xl">
-                            Kategori yang tersedia
-                        </h1>
-                    </div>
-
-                                     
-                    <div className="px-[30px] justify-between flex flex-wrap gap-x-1 gap-y-3">
-
-                        <List_Kategori>
-                            Ayam
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Kopi
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Sapi
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Babi
-                        </List_Kategori>
-                        
-                        <List_Kategori>
-                            Sayuran
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Buah
-                        </List_Kategori>
-                        
-                        <List_Kategori>
-                            Kambing
-                        </List_Kategori>
-
-                        <List_Kategori>
-                            Bekicot
-                        </List_Kategori>
-                        
-                    </div>
-
-                </div>
+          <div className="px-[30px] py-4">
+            <h1 className="font-semibold text-xl">
+                Kategori yang dipilih
+                </h1>
             </div>
-        </>
-    )
-}
+
+            {/* Kategori Terpilih */}
+            <div className="px-[30px]  flex flex-wrap gap-x-1 gap-y-3">
+              
+              {kategoriTerpilih.map((kategori, index) => (
+                <button
+                  key={index}
+                  className="border-2 border-[#EC7505] rounded-xl flex justify-center px-3 
+                            bg-[#EC7505] text-white"
+                  onClick={() => hapusKategoriTerpilih(kategori)}
+                >
+                  <h1 className="font-semibold font-poppins text-[12px]">
+                    {kategori}
+                  </h1>
+                </button>
+              ))}
+
+            </div>
+            
+            
+
+            <div className="px-[30px] py-4">
+              <h1 className="font-semibold text-xl">
+                Kategori yang tersedia
+              </h1>
+            </div>
+            
+            {/* Kategori Tersedia */}
+            <div className="px-[30px]  flex flex-wrap gap-x-1 gap-y-3">
+              {daftarKategori.map((kategori, index) => (
+                <button
+                  key={index}
+                  className={`border-2 border-[#EC7505] rounded-xl flex justify-center px-3 ${
+                    kategoriTerpilih.includes(kategori) ? "bg-blue-500 text-white" : ""
+                  }`}
+                  onClick={() => tambahKategoriTerpilih(kategori)}
+                >
+                  <h1 className="font-semibold font-poppins text-[12px]">
+                    {kategori}
+                  </h1>
+                </button>
+              ))}
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default add_kategori;
