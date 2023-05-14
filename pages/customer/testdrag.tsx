4363aa53ba6bee6dfef312f9d8a40a330de23465
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-05-14 20:52:30 +0700

    Update testdrag.tsx

--- pages/customer/testdrag.tsx
+++ pages/customer/testdrag.tsx
@@ -5,8 +5,16 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function TestDrag() {
   const [isDragged, setIsDragged] = useState(false);
 
-  const handleDrag = (e, ui) => {
+  const handleDrag = (e: any, ui: any) => {
     if (ui.lastY <= 10) {
+      setIsDragged(true);
+    } else {
+      setIsDragged(false);
+    }
+  };
+
+  const handleLoremDrag = (e: any, ui: any) => {
+    if (ui.lastY <= 0) {
       setIsDragged(true);
     } else {
       setIsDragged(false);
@@ -16,7 +24,7 @@ export default function TestDrag() {
   return (
     <div className="relative h-screen">
       {/* The first draggable area */}
-      <Draggable axis="y" onDrag={handleDrag}>
+      <Draggable axis="y" onDrag={handleDrag} bounds={{ bottom: 0 }}>
         <div className="bg-gray-400 flex flex-col justify-center items-center absolute bottom-0 left-0 right-0 cursor-pointer">
           <h1>Drag me!</h1>
 
@@ -30,12 +38,14 @@ export default function TestDrag() {
                 exit={{ height: 0 }}
                 transition={{ duration: 0.3 }}
               >
-                <div className="bg-gray-300 p-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  semper sed elit quis hendrerit. Morbi euismod felis vel mauris
-                  semper, vel mollis nunc pretium. Donec sollicitudin justo in mi
-                  tincidunt, at ultrices purus euismod.
-                </div>
+                <Draggable axis="y" onDrag={handleLoremDrag} bounds={{ bottom: 10 }} scale={1000}>
+                  <div className="bg-gray-300 p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                    semper sed elit quis hendrerit. Morbi euismod felis vel mauris
+                    semper, vel mollis nunc pretium. Donec sollicitudin justo in mi
+                    tincidunt, at ultrices purus euismod.
+                  </div>
+                </Draggable>
               </motion.div>
             )}
           </AnimatePresence>
