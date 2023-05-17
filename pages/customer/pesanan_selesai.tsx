018e17d94defd525b8f80f2458dbca8ef9ea10c8
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-05-17 07:45:10 +0700

    add pesanan_selesai

---
+++ pages/customer/pesanan_selesai.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link"
+
+export default function pesanan_selesai () {
+    return (
+        <div className="font-poppins">
+            <div className="bg-[#EC7505] h-screen w-screen">
+                <div className="bg-white rounded-t-3xl w-screen translate-y-[295px]">
+                    <div className="shadow-md mx-auto bg-white w-[300px] pb-[45px] rounded-3xl
+                                    -translate-y-[124px]">
+                        <img src="/b_pembhasil_centang.svg" className="mx-auto pt-[53px] pb-[10px]" />
+                        
+                        <h1 className="font-medium text-[23px] text-center">Pesanan Selesai</h1>
+
+                        {/* Tanggal Transaksi */}
+                        <h1 className="text-[18px] text-[#9A9A9A] text-center mt-[6px]">
+                            11 Maret 2023 09:00
+                        </h1>
+
+                        {/* Kode Transaksi */}
+                        <h1 className="text-[18px] text-[#9A9A9A] text-center">
+                            APX123JOASKDO231
+                        </h1>
+
+                        {/* Border Pembatas */}
+                        <div className="border-2 border-[#D9D9D9] w-[250px] mx-auto my-[15px]"></div>
+
+                        {/* Nama Resto*/}
+                        <h1 className="text-[18px] text-[#838080] font-medium pl-[33px]">
+                            Anteiku Coffee
+                        </h1>
+                        
+                        {/* Menu */}
+                        <div className="flex-col">
+                            <div className="flex justify-between text-[16px] text-[#9A9A9A] pl-[33px] pr-[32px]">
+                                {/* Nama Makanan */}
+                                <h1>
+                                    Kopi Hitam
+                                </h1>
+
+                                {/* Jumlah Makanan */}
+                                <p>
+                                    2
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Lanjutkan */}
+                        <Link href="/customer/hero">
+                            <button className="mt-[60px] w-[250px] h-[56px] rounded-full mx-auto
+                                                flex justify-center items-center bg-[#EC7505]">
+                                <h1 className="font-semibold text-[21px] text-white">Lanjutkan</h1>
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
