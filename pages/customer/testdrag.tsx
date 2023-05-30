985bd63b91b3ce06aefd34e9fe19a7927a1ba3e2
billyqodama <billy.fahd.qodama@mail.ugm.ac.id>
2023-05-30 20:49:44 +0700

    Update testdrag.tsx

--- pages/customer/testdrag.tsx
+++ pages/customer/testdrag.tsx
@@ -1,56 +1,103 @@
+import Image from "next/image";
 import { useState } from "react";
 import Draggable from "react-draggable";
 import { motion, AnimatePresence } from "framer-motion";
 
-export default function TestDrag() {
-  const [isDragged, setIsDragged] = useState(false);
-
-  const handleDrag = (e: any, ui: any) => {
-    if (ui.lastY <= 10) {
-      setIsDragged(true);
-    } else {
-      setIsDragged(false);
-    }
-  };
-
-  const handleLoremDrag = (e: any, ui: any) => {
-    if (ui.lastY <= 0) {
-      setIsDragged(true);
-    } else {
-      setIsDragged(false);
-    }
-  };
+const Order = () => {
+  const [statusOrder, setStatusOrder] = useState("Sedang menuju ke Restoran");
 
   return (
-    <div className="relative h-screen">
-      {/* The first draggable area */}
-      <Draggable axis="y" onDrag={handleDrag} bounds={{ bottom: 0 }}>
-        <div className="bg-gray-400 flex flex-col justify-center items-center absolute bottom-0 left-0 right-0 cursor-pointer">
-          <h1>Drag me!</h1>
-
-          {/* The content */}
-          <AnimatePresence>
-            {isDragged && (
-              <motion.div
-                className="w-full bg-gray-600"
-                initial={{ height: 0 }}
-                animate={{ height: "auto" }}
-                exit={{ height: 0 }}
-                transition={{ duration: 0.3 }}
-              >
-                <Draggable axis="y" onDrag={handleLoremDrag} bounds={{ bottom: 10 }} scale={1000}>
-                  <div className="bg-gray-300 p-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                    semper sed elit quis hendrerit. Morbi euismod felis vel mauris
-                    semper, vel mollis nunc pretium. Donec sollicitudin justo in mi
-                    tincidunt, at ultrices purus euismod.
-                  </div>
-                </Draggable>
-              </motion.div>
-            )}
-          </AnimatePresence>
+    <div>
+      <div className="h-25 w-full my-5 px-5">
+        <div className="flex items-center">
+          {/* <Image
+            src={"/images/icons/left-arrow.svg"}
+            alt="plus-icon"
+            width={26}
+            height={26}
+          /> */}
+          <h2 className="text-3xl font-semibold text-gray-600 ml-5">
+            {statusOrder}
+          </h2>
         </div>
-      </Draggable>
+        <div className="flex my-5 mx-1 justify-between">
+          {/* <Image src={"/images/icons/Location.svg"} width={23} height={35} /> */}
+          <div>
+            <p className="text-base font-semibold text-gray-700">Tempat</p>
+            <h3 className="text-lg font-bold">McDonalds cabang Kaliurang</h3>
+          </div>
+          <div>
+            <p className="text-base font-semibold">Estimasi</p>
+            <h3 className="text-lg text-gray-500 font-semibold">20 mnt</h3>
+          </div>
+        </div>
+      </div>
+      <Drag />
     </div>
   );
-}
+};
+
+export default Order;
+
+const Drag = () => {
+    const [isDragged, setIsDragged] = useState(false);
+    const [dragPosition, setDragPosition] = useState({ x: 0, y: 0 });
+  
+    const handleDrag = (e: any, ui: any) => {
+      if (ui.y >= -100 && ui.y <= 30) {
+        setIsDragged(true);
+        setDragPosition({ x: 0, y: ui.y });
+      } else {
+        setIsDragged(false);
+      }
+    };
+  
+    const handleLoremDrag = (e: any, ui: any) => {
+      if (ui.y >= 0 && ui.y <= 10) {
+        setIsDragged(true);
+      } else {
+        setIsDragged(false);
+      }
+    };
+  
+    return (
+      <div className="w-full h-full bg-black">
+        <Draggable
+          axis="y"
+          onDrag={handleDrag}
+          position={dragPosition}
+          bounds={{ top: -150, bottom: 0 }}
+        >
+          <div className="bg-gray-100 flex flex-col justify-center items-center absolute bottom-30 left-0 right-0 cursor-pointer">
+            <h1>Drag me!</h1>
+  
+            {/* The content */}
+            <AnimatePresence>
+              {isDragged && (
+                <motion.div
+                  className="w-full bg-gray-600"
+                  initial={{ height: 0 }}
+                  animate={{ height: "auto" }}
+                  exit={{ height: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <Draggable
+                    axis="y"
+                    onDrag={handleLoremDrag}
+                    bounds={{ top: 0, bottom: 10 }}
+                  >
+                    <div className="bg-gray-300 p-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Duis semper sed elit quis hendrerit. Morbi euismod felis vel
+                      mauris semper, vel mollis nunc pretium. Donec sollicitudin
+                      justo in mi tincidunt, at ultrices purus euismod.
+                    </div>
+                  </Draggable>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        </Draggable>
+      </div>
+    );
+  };
